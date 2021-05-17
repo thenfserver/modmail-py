7b1fd1ec6b0ca059d9063909be6138aa26cab6ea
Iron <ironjayk@gmail.com>
2021-05-17 14:53:13 -0400

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -17,7 +17,7 @@ class modmail(commands.Cog):
           embed = discord.Embed(title="Message Received", description=message.content,color=discord.Color.red(),timestamp=message.created_at)
           embed.set_author(name=f"{message.author}", icon_url=message.author.avatar_url)
           embed.set_footer(text=f"{message.author} | {message.author.id}")
-          await self.client.get_guild(475869084169273344).get_channel(764315204422991882).send(embed=embed)
+          await self.client.get_guild(GUILD_ID_HERE).get_channel(CHANNEL_ID_HERE).send(embed=embed)
           
           embed=discord.Embed(title="Message Sent", description=message.content, color=discord.Color.green(), timestamp=message.created_at)
           await message.author.send(embed=embed)
